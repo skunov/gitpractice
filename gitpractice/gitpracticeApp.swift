e1316d12fb5a43c0ae8b8d667404a093cc959fea
Sophia Kunov <kunov.sophia@gmail.com>
2025-08-06 11:29:52 -0700

    Initial Commit

---
+++ gitpractice/gitpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitpracticeApp.swift
+//  gitpractice
+//
+//  Created by Scholar on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct gitpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
