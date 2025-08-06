17d82023bf28b6ff481ba38b9828a2e5df663282
Sophia Kunov <kunov.sophia@gmail.com>
2025-08-06 11:35:17 -0700

    Added in change 1

--- gitpractice/ContentView.swift
+++ gitpractice/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Text("This is Change 1")
         }
         .padding()
     }
